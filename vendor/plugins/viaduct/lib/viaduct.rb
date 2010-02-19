d0b92b78112c909e210e2b1fc79b9adc56f8cce3
Michal Olah <olahmichal@gmail.com>
2010-02-19 15:35:36 +0100

    updates to viaduct. main update to associations.

--- vendor/plugins/viaduct/lib/viaduct.rb
+++ vendor/plugins/viaduct/lib/viaduct.rb
@@ -16,24 +16,27 @@ module Viaduct
     def new
       @model = model_class.new
       @fields = fields
+      @associations = model_class.reflect_on_all_associations
       render :template => 'viaduct/new'
     end
     
     def show
        @model = model_class.find(params[:id])
        @fields = fields
+       @associations = model_class.reflect_on_all_associations
        render :template => 'viaduct/show'
     end
  
     def edit
       @model = model_class.find(params[:id])
       @fields = fields
+      @associations = model_class.reflect_on_all_associations
       render :template => 'viaduct/edit'
     end
  
     def create
-      @model = model_class.new(params[:person])
-      if @model.save
+      @model = model_class.new(params[model_class.class_name.downcase.to_sym])
+      if @model.save && update_associations(@model)
         flash[:notice] = "#{model_class} was successfully created."
         redirect_to(@model)
       else
@@ -44,7 +47,7 @@ module Viaduct
     def update
       @model = model_class.find(params[:id])
  
-      if @model.update_attributes(params[:person])
+      if @model.update_attributes(params[model_class.class_name.downcase.to_sym]) && update_associations(@model)
         flash[:notice] = "#{model_class} was successfully updated."
         redirect_to(@model)
       else
@@ -59,6 +62,14 @@ module Viaduct
     end
  
     protected
+      def update_associations(model)
+        model_class.reflect_on_all_associations.each do |association|
+          model.send("#{association.class_name.downcase}=".to_sym,
+            AudioRecording.find(params[model_class.class_name.downcase.to_sym][association.class_name.downcase.to_sym]))
+        end
+        model.save
+      end
+    
       def model_class
         ActiveSupport::Inflector::classify(self.class.to_s.gsub(/Controller$/, '')).constantize
       end
